12fd9826cba9ed1a985374fea7cb36678f5ebe01
SUHAIB <suhaibpa72@gmail.com>
2023-02-05 19:54:50 +0530

    some changes in textfeld and column

--- lib/hivelogin.dart
+++ lib/hivelogin.dart
@@ -3,42 +3,53 @@ import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
-class loginscr extends StatefulWidget {
-  const loginscr({Key? key}) : super(key: key);
+class loginscr extends StatelessWidget {
+  loginscr({Key? key}) : super(key: key);
 
-  @override
-  State<loginscr> createState() => _loginscrState();
-}
-
-class _loginscrState extends State<loginscr> {
   final textcontroller = TextEditingController();
+
   final textcontroller1 = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          TextFormField(
-            controller: textcontroller,
-            decoration: const InputDecoration(
-              hintText: "enter your Gmail",
-              border: OutlineInputBorder(),
+      body: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: textcontroller,
+                decoration: const InputDecoration(
+                  hintText: "enter your name",
+                  border: OutlineInputBorder(),
+                ),
+              ),
             ),
-          ),
-          TextFormField(
-            controller: textcontroller1,
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              hintText: "enter your password",
+            const SizedBox(
+              height: 5,
             ),
-          ),
-          ElevatedButton.icon(
-            onPressed: () {},
-            icon: const Icon(Icons.check),
-            label: const Text("login"),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextFormField(
+                controller: textcontroller1,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: "enter your age",
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            ElevatedButton.icon(
+              onPressed: () {},
+              icon: const Icon(Icons.check),
+              label: const Text("login"),
+            ),
+          ],
+        ),
       ),
     );
   }

8685204a3615e6191500acd6d68cde0cc0205501
SUHAIB <suhaibpa72@gmail.com>
2023-02-06 20:59:48 +0530

    registration of database and the main function will converted to future function

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hive_implimentation/hivelogin.dart';
 import 'package:hive_implimentation/homescreen.dart';
+import 'package:hive_implimentation/model/data_model.dart';
 
-main() {
+Future<void> main() async {
+  await Hive.initFlutter();
+  if (!Hive.isAdapterRegistered(StudentAdapter().typeId)) {
+    Hive.registerAdapter(StudentAdapter());
+  }
   runApp(hiveapp());
 }
 
@@ -16,9 +22,7 @@ class hiveapp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData(
-        primaryColor: const Color.fromARGB(255, 7, 255, 205)
-      ),
+      theme: ThemeData(primaryColor: const Color.fromARGB(255, 7, 255, 205)),
       home: const home(),
     );
   }

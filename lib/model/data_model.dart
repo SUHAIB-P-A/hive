72ef0797074cc1ba042c7ee8901880651fc11fac
SUHAIB <suhaibpa72@gmail.com>
2023-02-06 20:52:41 +0530

    the list model changed to database table model

--- lib/model/data_model.dart
+++ lib/model/data_model.dart
@@ -1,6 +1,14 @@
+import 'package:hive_flutter/hive_flutter.dart';
+part 'data_model.g.dart';
+
+@HiveType(typeId: 1)
 class Student {
+  @HiveField(0)
+  int? id;
+  @HiveField(1)
   final String name;
+  @HiveField(2)
   final String age;
 
-  Student({required this.name, required this.age});
+  Student({required this.name, required this.age,this.id});
 }

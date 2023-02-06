29f7fede9e58f14d69871aa015699342b95e9a77
SUHAIB <suhaibpa72@gmail.com>
2023-02-06 21:03:40 +0530

    add a function call starting of the page the function works 'value getting the database and dispaly ui to restart the application'

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:hive_implimentation/functions/db_functions.dart';
 import 'package:hive_implimentation/hivelogin.dart';
 import 'package:hive_implimentation/liststudent.dart';
 
@@ -10,6 +11,7 @@ class home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    getstudentlist();
     return Scaffold(
       body: SafeArea(
         child: Column(

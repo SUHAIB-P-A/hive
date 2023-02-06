81473a30bc89c10138be40d6acd44482f6276c1e
SUHAIB <suhaibpa72@gmail.com>
2023-02-06 20:56:58 +0530

    add tow functions and a small change in value adding function. the tow functions are value getting and deleting for database values

--- lib/functions/db_functions.dart
+++ lib/functions/db_functions.dart
@@ -1,10 +1,27 @@
 import 'package:flutter/cupertino.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:hive_implimentation/model/data_model.dart';
 
 ValueNotifier<List<Student>> studentlistnotifier = ValueNotifier([]);
 
-void addstudentlist(Student value) {
+Future<void> addstudentlist(Student value) async {
+  final student_db = await Hive.openBox<Student>('studentDB');
+  final _id = await student_db.add(value);
+  value.id = _id;
   studentlistnotifier.value.add(value);
-  print(value.toString());
+  //print(value.toString());
   studentlistnotifier.notifyListeners();
+}
+
+Future<void> getstudentlist() async {
+  final student_db = await Hive.openBox<Student>('studentDB');
+  studentlistnotifier.value.clear();
+  studentlistnotifier.value.addAll(student_db.values);
+  studentlistnotifier.notifyListeners();
+}
+
+Future<void> deletestudentlist(int id) async {
+  final student_db = await Hive.openBox<Student>('studentDB');
+  await student_db.delete(id);
+  getstudentlist();
 }
